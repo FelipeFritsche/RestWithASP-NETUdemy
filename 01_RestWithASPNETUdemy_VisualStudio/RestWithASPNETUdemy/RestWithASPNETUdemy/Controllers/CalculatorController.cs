cb57b8503b03593401f84eeb1bafc5b2cd6c7371
Felipe Fritsche <Felipe.Fritsche@lollybaby.com.br>
2021-07-28 11:40:18 -0300

    Implementing calculator

---
+++ 01_RestWithASPNETUdemy_VisualStudio/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CalculatorController : ControllerBase
+  {
+
+
+    [HttpGet("sum/{firstNumber}/{secondNumber}")]
+    public IActionResult Get(string firstNumber, string secondNumber)
+    {
+      if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+      {
+        var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+        return Ok(sum.ToString());
+      }
+      return BadRequest("Invalid Input");
+    }
+
+    private decimal ConvertToDecimal(string strNumber)
+    {
+      decimal decimalValue;
+      if (decimal.TryParse(strNumber, out decimalValue))
+      {
+        return decimalValue;     
+      }
+      return 0;
+    }
+
+    private bool IsNumeric(string strNumber)
+    {
+     double number;
+     bool isNumber = double.TryParse(
+          strNumber, System.Globalization.NumberStyles.Any, 
+          System.Globalization.NumberFormatInfo.InvariantInfo, 
+          out number);
+      return isNumber;
+      throw new NotImplementedException();
+    }
+  }
+}
